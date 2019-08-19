3a60a7a3ecc6f9c6dc43a619db15f28dea699ca0
projectsaathi01 <projectsaathi@gmail.com>
2019-08-19 18:43:48 +0545

    app for home automation version 1.0

---
+++ app/src/main/java/com/example/meroghar/BedroomFragment.java
@@ -0,0 +1,49 @@
+package com.example.meroghar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class BedroomFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragement_bedroom, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.on).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("acStatus");
+
+                myRef.setValue(1);
+            }
+        });
+
+        view.findViewById(R.id.off).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("acStatus");
+
+                myRef.setValue(0);
+            }
+        });
+    }
+}
