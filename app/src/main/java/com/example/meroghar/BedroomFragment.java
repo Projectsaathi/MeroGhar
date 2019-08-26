17df36fba9bf5c6cfdcd4a8ece91d05f48b4c2cf
projectsaathi01 <projectsaathi@gmail.com>
2019-08-26 19:30:41 +0545

    all bedroom appliances added

--- app/src/main/java/com/example/meroghar/BedroomFragment.java
+++ app/src/main/java/com/example/meroghar/BedroomFragment.java
@@ -26,6 +26,7 @@ public class BedroomFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+//        code for AC
         view.findViewById(R.id.on).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +42,70 @@ public class BedroomFragment extends Fragment {
             public void onClick(View view) {
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("acStatus");
+
+                myRef.setValue(0);
+            }
+        });
+
+//        code for light
+        view.findViewById(R.id.light_on).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("ledStatus");
+
+                myRef.setValue(1);
+            }
+        });
+
+        view.findViewById(R.id.light_off).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("ledStatus");
+
+                myRef.setValue(0);
+            }
+        });
+
+//        Code for Fan
+        view.findViewById(R.id.fan_on).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("fanStatus");
+
+                myRef.setValue(1);
+            }
+        });
+
+        view.findViewById(R.id.fan_off).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("fanStatus");
+
+                myRef.setValue(0);
+            }
+        });
+
+//        Code for tv
+
+        view.findViewById(R.id.tv_on).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("tvStatus");
+
+                myRef.setValue(1);
+            }
+        });
+
+        view.findViewById(R.id.tv_off).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("tvStatus");
 
                 myRef.setValue(0);
             }
